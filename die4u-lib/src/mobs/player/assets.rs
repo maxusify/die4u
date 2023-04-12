ddf55ec02944e1ff564c45a1629c7e557f6a5f42
maxusify <maxusify@gmail.com>
2023-04-12 17:27:05 +0200

    Add new player assets

--- die4u-lib/src/mobs/player/assets.rs
+++ die4u-lib/src/mobs/player/assets.rs
@@ -4,5 +4,9 @@ use bevy_asset_loader::prelude::*;
 #[derive(AssetCollection, Resource)]
 pub struct PlayerAssets {
     #[asset(key = "player.idle")]
-    pub player_idle: Handle<TextureAtlas>,
+    pub idle: Handle<TextureAtlas>,
+    #[asset(key = "player.walk")]
+    pub walk: Handle<TextureAtlas>,
+    #[asset(key = "player.run")]
+    pub run: Handle<TextureAtlas>,
 }
--- die4u-lib/src/mobs/player/systems.rs
+++ die4u-lib/src/mobs/player/systems.rs
@@ -12,9 +12,9 @@ use super::components::Player;
 
 /// Spawns default player as [`RigidBody`]
 pub fn spawn_default_player_with_physics(
-  mut commands: Commands,
-  assets: Res<PlayerAssets>,
-  input_map: Res<GamePlayerInput>
+    mut commands: Commands,
+    assets: Res<PlayerAssets>,
+    input_map: Res<GamePlayerInput>,
 ) {
     commands
         .spawn((
@@ -22,18 +22,24 @@ pub fn spawn_default_player_with_physics(
             DefaultMobBundle {
                 sprite: SpriteSheetBundle {
                     sprite: TextureAtlasSprite::new(0),
-                    texture_atlas: assets.player_idle.clone(),
+                    texture_atlas: assets.idle.clone(),
+                    transform: Transform {
+                        translation: Vec3::new(0.0, 0.0, 0.5),
+                        scale: Vec3::splat(2.0),
+                        ..default()
+                    },
                     ..Default::default()
                 },
                 ..Default::default()
             },
             // Inputs
             InputManagerBundle::<PlayerActions> {
-              input_map: input_map.input_map.clone(),
-              ..default()
+                input_map: input_map.input_map.clone(),
+                ..default()
             },
             // Physics
             RigidBody::Dynamic,
+            Velocity::default(),
             ExternalForce::default(),
             ExternalImpulse::default(),
         ))
@@ -41,29 +47,57 @@ pub fn spawn_default_player_with_physics(
             children
                 .spawn(Collider::cuboid(10.0, 20.0))
                 .insert(TransformBundle::from(Transform::from_xyz(0.0, -10.0, 0.0)))
-                .insert(ColliderMassProperties::Mass(3.0));
+                .insert(ColliderMassProperties::Mass(2.0));
         });
 }
 
 /// Move player with physics
 pub fn player_movement_with_physics(
-  mut external_force: Query<&mut ExternalForce, With<Player>>,
-  actions: Query<&ActionState<PlayerActions>, With<Player>>,
+    mut external_force: Query<&mut ExternalForce, With<Player>>,
+    mut player_query: Query<
+        (
+            &ActionState<PlayerActions>,
+            &Velocity,
+            &mut TextureAtlasSprite,
+            &mut Handle<TextureAtlas>,
+        ),
+        With<Player>,
+    >,
+    player_assets: Res<PlayerAssets>,
 ) {
     let mut old_force = external_force.single_mut();
     let mut new_force = Vec2::splat(0.0);
-    let action_state = actions.single();
+    let (action_state, velocity, mut player_texture, mut player_atlas_sprite) =
+        player_query.single_mut();
 
     // Process keys
     let _ = action_state
         .get_pressed()
         .iter()
-        .map(|key| {
-            match key {
-                PlayerActions::Jump => new_force.y += 750.0,
-                PlayerActions::MoveLeft => new_force.x -= 500.0,
-                PlayerActions::MoveRight => new_force.x += 500.0,
-                _ => (),
+        .map(|key| match key {
+            PlayerActions::Jump => {
+                new_force.y += 750.0;
+            }
+            PlayerActions::MoveLeft => {
+                new_force.x -= 500.0;
+                if velocity.linvel.x.abs() > 500.0 {
+                    *player_atlas_sprite = player_assets.run.clone();
+                } else {
+                    *player_atlas_sprite = player_assets.walk.clone();
+                }
+                player_texture.flip_x = true;
+            }
+            PlayerActions::MoveRight => {
+                new_force.x += 500.0;
+                if velocity.linvel.x.abs() > 500.0 {
+                    *player_atlas_sprite = player_assets.run.clone();
+                } else {
+                    *player_atlas_sprite = player_assets.walk.clone();
+                }
+                player_texture.flip_x = false;
+            }
+            _ => {
+                *player_atlas_sprite = player_assets.idle.clone();
             }
         })
         .collect::<Vec<()>>();
